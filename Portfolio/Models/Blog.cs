75a6e925aa0417f2ed9956f1c912fbe3ec67ee0c
RNewsom <ryanjnewsom@gmail.com>
2017-03-11 12:44:30 -0700

    Cleaned up blog index

--- Portfolio/Models/Blog.cs
+++ Portfolio/Models/Blog.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Portfolio.Models
 {
@@ -9,6 +7,8 @@ namespace Portfolio.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Label { get; set; }
         public string Body { get; set; }
