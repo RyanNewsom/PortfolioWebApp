00892860383a3fdc7ce98b25ce4cead17cf56528
RNewsom <ryanjnewsom@gmail.com>
2017-03-11 20:15:37 -0700

    looking pretty good

--- Portfolio/Models/Blog.cs
+++ Portfolio/Models/Blog.cs
@@ -6,11 +6,16 @@ namespace Portfolio.Models
     public class Blog
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Label { get; set; }
+        [Required]
         public string Body { get; set; }
     }
 }
